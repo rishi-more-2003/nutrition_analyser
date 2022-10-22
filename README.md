59f9bfe51dc629620773bf60ee058956eeedf48e
Rishi More <rishimore102@gmail.com>
2022-10-22 20:21:17 +0530

    Updated for deployment

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nutrition_analyser
+web: gunicorn food_viser.food_viser.wsgi
--- food_viser/food_viser/wsgi.py
+++ food_viser/food_viser/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_viser.settings")
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
