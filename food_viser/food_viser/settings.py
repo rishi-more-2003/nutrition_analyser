3370940417d04ef52638b57cc6ec1a649b10ca8a
Rishi More <rishimore102@gmail.com>
2022-10-22 18:02:55 +0530

    Moved static files

--- food_viser/food_viser/settings.py
+++ food_viser/food_viser/settings.py
@@ -118,9 +118,6 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
